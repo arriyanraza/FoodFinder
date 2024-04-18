7e6bb820c0cd63f0981ac79f6d7dfb1613b86711
adi-granados <122556998+adi-granados@users.noreply.github.com>
2024-04-18 17:52:56 -0400

    Add files via upload

--- dataset/my-recipe-app/src/App.js
+++ dataset/my-recipe-app/src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 function App() {
   const [ingredients, setIngredients] = useState('');
   const [recipes, setRecipes] = useState([]);
-  const [isVegetarian, setIsVegetarian] = useState(false);  // Tracks the vegetarian toggle state
+  const [isVegetarian, setIsVegetarian] = useState(false);
 
   const fetchRecipes = async () => {
     try {
-      const ingredientsText = ingredients.split(/[, ]+/).filter(Boolean).join(' '); 
+      const ingredientsText = ingredients.split(/[, ]+/).filter(Boolean).join(' ');
       const requestBody = {
         ingredients: ingredientsText,
         vegetarian: isVegetarian
       };
-  
+
       const response = await fetch('http://127.0.0.1:5000/api/recipes', {
         method: 'POST',
         headers: {
@@ -21,18 +21,18 @@ function App() {
         },
         body: JSON.stringify(requestBody)
       });
-  
+
       const data = await response.json();
       setRecipes(data);
-    } catch (error) {
+    } catch ( error ) {
       console.error('Failed to fetch recipes:', error);
     }
   };
 
-
   return (
     <div className="App">
       <header className="App-header">
+        <h1>The Food Rec</h1>
         <input
           type="text"
           value={ingredients}
@@ -50,15 +50,21 @@ function App() {
         </div>
         <label htmlFor="veg-toggle">Vegetarian Only</label>
         <button onClick={fetchRecipes}>Get Recipes</button>
-        <ul>
-    {recipes.map((recipe, index) => (
-        <li key={index}>
-            <h3>{recipe.title} - Matches: {recipe.score.toFixed(2)}</h3>
-            <p>Ingredients: {recipe.ingredients.join(', ')}</p>
-            <p>{recipe.is_vegetarian ? "Suitable for Vegetarians" : "Includes Meat"}</p>
-            <a href={recipe.link} target="_blank" rel="noopener noreferrer">View Recipe</a>
-        </li>
-    ))}
+        <ul className="recipe-list">
+  {recipes.map((recipe, index) => (
+    <li key={index} className="recipe-item">
+      <img src={recipe.image} alt={recipe.title} style={{ width: '200px', height: '200px' }} />
+      <h3>{recipe.title} - Matches: {recipe.score.toFixed(2)}</h3>
+      <div>Ingredients:</div>
+      <ul className="ingredients-list">
+        {recipe.ingredients.map((ingredient, idx) => (
+          <li key={idx}>{ingredient}</li>
+        ))}
+      </ul>
+      <p>{recipe.is_vegetarian ? "Suitable for Vegetarians" : "Includes Meat"}</p>
+      <a href={recipe.link} target="_blank" rel="noopener noreferrer">View Recipe</a>
+    </li>
+  ))}
 </ul>
 
       </header>
